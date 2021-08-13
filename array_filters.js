a48805de679b3e9953ef69c5af17c88191e61479
Kasatky <opacopa@inbox.ru>
2021-08-13 21:44:42 +0300

    Cats colours

--- array_filters.js
+++ array_filters.js
@@ -1,4 +1,13 @@
 let cats = [
-    { name: 'Rizhik' },
-    { name: 'Pushok' }
+    { name: 'Rizhik',colour: 'foxy' },
+    { name: 'Pushok', colour: 'black' },
+    { name: 'Snezhok', colour: 'black' }
 ]
+
+let foundCat = cats.find(
+    function (dog){
+        return dog.name == 'Pushok'
+    }
+)
+
+console.info(foundCat)
